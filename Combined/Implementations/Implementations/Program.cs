7d6138563b274b07b5ed24081a58834473f58381
Boris Kulagin <b.kulagin@gmail.com>
2024-05-11 22:18:40 +0300

    IMplementatios

--- Combined/Implementations/Implementations/Program.cs
+++ Combined/Implementations/Implementations/Program.cs
@@ -4,6 +4,10 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        FooBase fooBase = new();
+
+        string strRes = fooBase.Foo();
+
+        Console.WriteLine(strRes);
     }
 }
