a4e429a629543bc8c93de90645073927590d5c43
Boris Kulagin <b.kulagin@gmail.com>
2024-05-11 22:01:46 +0300

    AllStandartWithFullSyncStack

--- Combined/AllStandartWithFullSyncStack/AllStandartWithFullSyncStack/Program.cs
+++ Combined/AllStandartWithFullSyncStack/AllStandartWithFullSyncStack/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace AllStandartWithFullSyncStack;
+﻿using AllStandartWithFullSyncStack.GetResult;
+using AllStandartWithFullSyncStack.Result;
+using AllStandartWithFullSyncStack.Sync;
+using AllStandartWithFullSyncStack.Wait;
+
+namespace AllStandartWithFullSyncStack;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SyncClass syncClass = new();
+        string syncResult = syncClass.Foo();
+
+        _ = GetResultClass.Foo();
+
+        ResultClass resultClass = new();
+        string resResult = resultClass.Foo();
+
+        WaitClass.Foo();
     }
 }
