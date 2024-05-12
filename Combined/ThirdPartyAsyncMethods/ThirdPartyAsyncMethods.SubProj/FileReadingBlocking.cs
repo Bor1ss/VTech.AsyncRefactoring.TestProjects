3344489306ad1131b89d0735e455e95fde9c91d0
Boris Kulagin <b.kulagin@gmail.com>
2024-05-12 14:52:06 +0300

    cleanup

--- Combined/ThirdPartyAsyncMethods/ThirdPartyAsyncMethods.SubProj/FileReadingBlocking.cs
+++ Combined/ThirdPartyAsyncMethods/ThirdPartyAsyncMethods.SubProj/FileReadingBlocking.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ThirdPartyAsyncMethods.SubProj;
+﻿namespace ThirdPartyAsyncMethods.SubProj;
 public class FileReadingBlocking
 {
     public static string Foo()
--- Combined/ThirdPartyAsyncMethods/ThirdPartyAsyncMethods.SubProj/HttpClientBlocking.cs
+++ Combined/ThirdPartyAsyncMethods/ThirdPartyAsyncMethods.SubProj/HttpClientBlocking.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ThirdPartyAsyncMethods.SubProj;
+﻿namespace ThirdPartyAsyncMethods.SubProj;
 public class HttpClientBlocking
 {
     public static void Foo()
